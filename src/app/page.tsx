9c6818b799d2481ec286d2f02cc78775be5c6fe1
Niraaj10 <nitcode10@gmail.com>
2024-10-07 19:55:07 +0530

    HeroSection Created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import HeroSection from "@/components/HeroSection";
+
 export default function Home() {
   return (
     <>
       <main className="min-h-screen bg-black/[0.96] antialiased">
         <h1>HELLO I"MM NIRAAAAAAJ</h1>
+
+        <HeroSection />
+
       </main>
     </>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,26 @@
 import type { Config } from "tailwindcss";
 
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
+
+
+
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,12 +30,32 @@ const config: Config = {
   darkMode: "class",
   theme: {
     extend: {
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: "0",
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    addVariablesForColors,
+  ],
 };
+
+
+
 export default config;
